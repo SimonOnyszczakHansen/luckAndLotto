56c8de64d1fdc35a1359c8ab20eb83a242b6ba13
Simon Onyszczak Hansen <simo401t@zbc.dk>
2022-08-09 14:03:54 +0200

    Add project files.

---
+++ luckAndLotto/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace luckAndLotto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the user interface, where the user can press a key if he wants to run the code
+            Console.WriteLine("Tryk på enter for at trække dine numre");
+            Console.Read();
+
+            //Here i generate the random numbers
+            Random rand = new Random();
+            
+            //I made a loop that runs 7 times so we generate 7 numbers
+            for (int i = 0; i < 7; i++)
+            {
+                //This code makes the last number red
+                if (i == 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                //Here i generate the numbers, write them in console, and pause the code for 2 seconds
+                int number = rand.Next(1, 37);
+                Console.Write(number + " ");
+                Thread.Sleep(2000);        
+            }
+            Console.Read();
+        }
+    }
+}
